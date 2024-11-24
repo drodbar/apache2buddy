89c0c1a591a4cb5d6568b5fb7547caab5f278aa1
Diego Rodriguez <drodriguezb@civitatis.com>
2024-11-24 19:16:33 +0100

    refactor a2bck.sh

--- a2bchk.sh
+++ a2bchk.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-# example of testing md5sums prior to execution
 
-scriptmd5sum=`curl -sL https://raw.githubusercontent.com/richardforth/apache2buddy/master/apache2buddy.pl | md5sum | cut -d " " -f1`
-originmd5sum=`curl -s https://raw.githubusercontent.com/richardforth/apache2buddy/master/md5sums.txt | cut -d " " -f1`
-echo $scriptmd5sum
-echo $originmd5sum
-if [ $scriptmd5sum == $originmd5sum ]
-then
-        scriptsha256sum=`curl -sL https://raw.githubusercontent.com/richardforth/apache2buddy/master/apache2buddy.pl | sha256sum | cut -d " " -f1`
-        originsha256sum=`curl -s https://raw.githubusercontent.com/richardforth/apache2buddy/master/sha256sums.txt | cut -d " " -f1`
-        echo $scriptsha256sum
-        echo $originsha256sum
-        if [ $scriptsha256sum == $originsha256sum ]
-        then
-                # execute the code, its safe - we can assume
-                curl -sL https://raw.githubusercontent.com/richardforth/apache2buddy/master/apache2buddy.pl | perl
-        else
-                echo "Error: SHA256SUM mismatch, execution aborted."
-        fi
-else
-        echo "Error: MD5SUM mismatch, execution aborted."
+BRANCH='master'
+REPO_URL="https://raw.githubusercontent.com/drodbar/apache2buddy"
+FULL_URL="${REPO_URL}/${BRANCH}"
+TEMPILE="$(mktemp)"
+script="$(curl -sL ${FULL_URL}/apache2buddy.pl > "${TEMPILE}")"
+
+scriptmd5sum="$(md5sum "${script}" | cut -d " " -f1)"
+originmd5sum="$(curl -s ${FULL_URL}/md5sums.txt | cut -d " " -f1)"
+echo "md5sum [$originmd5sum] expected [$scriptmd5sum]"
+if [[ "$scriptmd5sum" != "$originmd5sum" ]]; then
+    echo "Error: MD5SUM mismatch, execution aborted."
+    exit 1
 fi
+
+scriptsha256sum="$(sha256sum "${script}"| cut -d " " -f1)"
+originsha256sum="$(curl -s ${FULL_URL}/sha256sums.txt | cut -d " " -f1)"
+echo "sha256sum [$originsha256sum] expected [$scriptsha256sum]"
+if [[ "$scriptsha256sum" != "$originsha256sum" ]]; then
+    echo "Error: SHA256SUM mismatch, execution aborted."
+    exit 1
+fi
+
+mv "${TEMPILE}" apache2buddy.pl
